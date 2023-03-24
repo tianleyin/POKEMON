9feaf5d940ad4ab31cfc6d7491669ab436ddffd4
martimiranda <120406341+martimiranda@users.noreply.github.com>
2023-03-24 20:13:58 +0100

    Update Pokemon.java

--- Pokemon.java
+++ Pokemon.java
@@ -1,50 +1,51 @@
 package pokemons;
-
 import java.util.Comparator;
-
-public abstract class Pokemon implements Comparator<Pokemon> {
-	private String nom, tipo_pokemon;
+public abstract class Pokemon implements Comparable<Pokemon> {
+	private String nom,tipo_pokemon;
 	private int energia,vida,danybase,defensa,precisio,vidaperm;
 	
-	public Pokemon(String tipo,String nombre){
+	Pokemon(String tipo,String nombre){
 		if(tipo.equals("aigua")) {
-			this.tipo_pokemon = tipo;
+			int vidaa = (int) (Math.random() * 150 + 250);
 			this.nom=nombre;
 			this.energia= 100;
-			this.vida= (int) (Math.random() * 150 + 250);
+			this.vida= vidaa;
 			this.danybase=15;
 			this.defensa=10;
 			this.precisio=80;
-			this.vidaperm=vida;
+			this.vidaperm=vidaa;
+			this.tipo_pokemon=tipo;
 		}
 		else if(tipo.equals("foc")) {
-			this.tipo_pokemon = tipo;
+			int vidaa = (int) (Math.random() * 150 + 250);
 			this.nom=nombre;
 			this.energia= 100;
-			this.vida= (int) (Math.random() * 150 + 250);
+			this.vida= vidaa;
 			this.danybase=20;
 			this.defensa=15;
 			this.precisio=72;
-			this.vidaperm=vida;
+			this.vidaperm=vidaa;
+			this.tipo_pokemon=tipo;
 		}
 		else if(tipo.equals("electric")) {
-			this.tipo_pokemon = tipo;
+			int vidaa = (int) (Math.random() * 150 + 250);
 			this.nom=nombre;
 			this.energia= 100;
-			this.vida= (int) (Math.random() * 150 + 250);
+			this.vida= vidaa;
 			this.danybase=25;
 			this.defensa=15;
 			this.precisio=63;
-			this.vidaperm=vida;
+			this.vidaperm=vidaa;
+			this.tipo_pokemon=tipo;
 		}
-	}
+		}
 	public void resetStats() {
 		this.energia=100;
 		this.vida=vidaperm;
 	}
 	public String getNombre(){
 		return this.nom;
-	}
+	} 
 	public int getVida() {
 		return this.vida;
 	}
@@ -63,9 +64,8 @@ public abstract class Pokemon implements Comparator<Pokemon> {
 			"Vida: " + this.vida+ ", "+ "Dany: " + this.danybase+ ", "+ 
 				"Defensa: " + this.defensa+ ", " + "Precisió: " + this.precisio);
 	}
-	
 	@Override
-	public int compare(Pokemon p1, Pokemon p2) {
-		return p1.getVida() - p2.getVida();
+	public int compareTo(Pokemon p1) {
+		return p1.getVida() - this.getVida();
 	}
 }
